20cb1e9eea4396515a4800c9eab8c51ea4f28b6b
phynix-x <stripathi.works@gmail.com>
2026-09-24 16:00:30 +0530

    Connect Trident frontend to Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://dqjlscvftofaqvglzuli.supabase.co',
+  'sb_publishable_I6okH-lszpeJPTc0dsaQXg_YGoQ5uJs'
+)
